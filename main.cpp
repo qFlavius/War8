ca6b66129f4c974d835c506cf158bf7513c205e3
Flavius <flavius.sandu@student.uaic.ro>
2025-12-10 22:12:59 +0200

    linked SFML

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-	std::cout << "hello\n";
-	return 0;
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
